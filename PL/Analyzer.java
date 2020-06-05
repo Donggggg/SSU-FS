1ce944ab7bcf7a61bc8db8efe234953a5cc6da7c
Donggggg <ptsaturn68@daum.net>
2020-06-05 21:46:43 +0900

    completed Midterm Project PL

--- PL/Analyzer.java
+++ PL/Analyzer.java
@@ -4,57 +4,57 @@ import java.io.FileReader;
 import java.io.IOException;
 
 public class Analyzer {
-	int nextChar;
-	int charClass;
-	int nextToken;
-	int lexLen;
-	char lexeme[] = new char[100];
-	FileReader file_reader;
+	int nextChar; // 토큰의 1바이트
+	int charClass; // charClass 변수[LETTER, DIGIT, UNKNOWN, EOF]
+	int nextToken; // 토큰의 종류 
+	int lexLen; // 토큰의 길이
+	char lexeme[] = new char[100]; // 토큰
+	FileReader file_reader; // 소스코드 파일 리더 
 
-	Analyzer(FileReader file_reader){
+	Analyzer(FileReader file_reader){ // 생성자 
 		this.file_reader = file_reader;
 	}
 
-	void addChar()
+	void addChar() // 토큰을 완성시키는 메소드
 	{
-		if(lexLen < 100){
-			lexeme[lexLen++] = (char)nextChar; 
-			for(int i = lexLen; i < 100; i++)
+		if(lexLen < 100){ // 최대길이를 초과하지 않으면
+			lexeme[lexLen++] = (char)nextChar; //토큰에 1바이트 이어붙임 
+			for(int i = lexLen; i < 100; i++) // 의미 없는 공간을 널로 세팅
 				lexeme[i] = '\0';
 		}
-		else
+		else // 예외 처리
 			System.out.println("Error - lexeme is too long");
 	}
 
-	void getChar()
+	void getChar() // 토큰을 구분하는 메소드
 	{
 		try{
-			if((nextChar = file_reader.read()) != -1){
-				if(Character.isLetter(nextChar))
+			if((nextChar = file_reader.read()) != -1){ // 파일에서 1바이트 입력 받음
+				if(Character.isLetter(nextChar)) // 알파벳(대, 소문자)인 경우 
 					charClass = Data.LETTER;
-				else if(Character.isDigit(nextChar))
+				else if(Character.isDigit(nextChar)) // 숫자(0~9)인 경우
 					charClass = Data.DIGIT;
-				else
+				else // 그 외의 경우
 					charClass = Data.UNKNOWN;
 			}
-			else
-				charClass = -1; // EOF
+			else // 파일의 끝이면 
+				charClass = -1; // EOF 세팅
 		}
-		catch(IOException e){
+		catch(IOException e){ // 예외처리
 			e.getStackTrace();
 		}
 	}
 
 	void getNonBlank()
 	{
-		while(Character.isWhitespace(nextChar))
+		while(Character.isWhitespace(nextChar)) // 공백 문자가 아닐 때까지 파일 탐색
 			getChar(); 
 	}
 
 	void lex()
 	{
-		lexLen = 0;
-		getNonBlank();
+		lexLen = 0; // 토큰 길이 0으로 세팅
+		getNonBlank(); // 공백문자 스킵
 
 		switch(charClass){
 
@@ -76,7 +76,7 @@ public class Analyzer {
 				}
 				nextToken = Data.INT_LIT;
 				break;                    
-			case 99 :
+			case 99 : // UNKNOWN
 				lookup((char)nextChar); // lookup 테이블 상에 존재하는 1바이트 기호인지 검사
 				getChar(); // 다음 문자 세팅
 				break;
@@ -85,11 +85,12 @@ public class Analyzer {
 				lexeme[0] = 'E';
 				lexeme[1] = 'O';
 				lexeme[2] = 'F';
-				lexeme[3] = '\0';
+				for(i = 3; i < 100; i++) // 의미 없는 공간 널문자 
+					lexeme[i] = '\0';
 				break;
 		}
 
-		String s = String.valueOf(lexeme);
+		String s = String.valueOf(lexeme); // char[] to String
 		System.out.println("Next Token is: " + nextToken + ", Next Lexeme is "+s);
 	}
 
--- PL/Main.java
+++ PL/Main.java
@@ -6,20 +6,20 @@ import java.io.IOException;
 
 public class Main {
 	public static void main(String[] args) {  
-		File file;
-		FileReader file_reader;
-		Analyzer analyzer;
+		File file; // 소스 코드 파일 
+		FileReader file_reader; // 소스 코드 파일리더
+		Analyzer analyzer; // 소스 분석을 할 Analyzer 객체 
 
 		try{
-			file = new File("/workspace/java_test/src/project/front.in");
-			file_reader = new FileReader(file);
-			analyzer = new Analyzer(file_reader);
-			analyzer.getChar();
+			file = new File("front.in"); // 소스코드 오픈
+			file_reader = new FileReader(file); // 파일리더 객체 생성
+			analyzer = new Analyzer(file_reader); // Analyzer 객체 생성
+			analyzer.getChar(); // 소스 코드 첫 데이터 분석..
 			do{
-				analyzer.lex();
-			}while(analyzer.nextToken != -1);
+				analyzer.lex(); // 토큰화 작업 
+			}while(analyzer.nextToken != -1); // EOF를 만날 때까지 분석 반복..
 		}
-		catch(IOException e){
+		catch(IOException e){ // 예외 처리
 			e.getStackTrace();
 		}     
 	}
